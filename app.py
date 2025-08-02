482205805de4921676f76593c34abe8c868df680
Kumari Divya Rashmi <kumaridivyarashmi306@gmail.com>
2025-08-02 23:56:21 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import spacy
+import os
+
+app = Flask(__name__)
+nlp = spacy.load("en_core_web_sm")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        job_description = request.form['jd']
+        # Assume some parsing and matching function here
+        result = "Sample result: skills match score 85%"
+        return render_template('index.html', result=result)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
